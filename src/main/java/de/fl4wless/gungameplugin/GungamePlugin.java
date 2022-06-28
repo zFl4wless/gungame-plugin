9bbf687835f4f1d113b0e931bf2d9ed5f923a5b5
zFl4wless <noah.bauditz@live.de>
2022-06-29 00:35:29 +0200

    Initial Commit

---
+++ src/main/java/de/fl4wless/gungameplugin/GungamePlugin.java
@@ -0,0 +1,17 @@
+package de.fl4wless.gungameplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GungamePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
